24b44b07dd0ae32dc88fcabddea540e14f0bc669
Walescko <walescko@gmail.com>
2022-10-11 20:31:04 -0300

    js/html/css refactory

--- WebDesign/js/lightDarkMode.js
+++ WebDesign/js/lightDarkMode.js
@@ -1,12 +1,4 @@
-function darkMode() {
-    let element = document.body;
-    let content = document.getElementById("DarkModetext");
-    element.className = "dark-mode";
-}
-
 function lightMode() {
     let element = document.body;
-    let content = document.getElementById("DarkModetext");
-    element.className = "light-mode";
+    element.classList.toggle("lightDark-mode");
 }
-
